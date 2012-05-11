1df32a8ededa0990fa3af9ab7b6ce03306d5e3d0
Jesse Luehrs <doy@tozt.net>
2012-05-10 21:07:21 -0500

    add RunExtraTests plugin

---
+++ lib/Dist/Zilla/Plugin/RunExtraTests.pm
@@ -0,0 +1,70 @@
+use strict;
+use warnings;
+package Dist::Zilla::Plugin::RunExtraTests;
+# ABSTRACT: support running xt tests via dzil test
+# VERSION
+
+# Dependencies
+use Dist::Zilla 2.100950 (); # XXX really the next release after this date
+use App::Prove 3.00 ();
+use Moose 0.99;
+use namespace::autoclean 0.09;
+
+# extends, roles, attributes, etc.
+
+with 'Dist::Zilla::Role::TestRunner';
+
+# methods
+
+sub test {
+  my $self = shift;
+
+  my @dirs;
+  push @dirs, 'xt/release' if $ENV{RELEASE_TESTING};
+  push @dirs, 'xt/author'  if $ENV{AUTHOR_TESTING};
+  push @dirs, 'xt/smoke'   if $ENV{AUTOMATED_TESTING};
+  @dirs = grep { -d } @dirs;
+  return unless @dirs;
+
+  my @builders = @{ $self->zilla->plugins_with(-BuildRunner) };
+  die "no BuildRunner plugins specified" unless @builders;
+  $builders[0]->build;
+
+  my $app = App::Prove->new;
+  $app->process_args(qw/-r -b/, @dirs);
+  $app->run or $self->log_fatal("Fatal errors in xt tests");
+  return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=for Pod::Coverage::TrustPod
+    test
+
+=begin wikidoc
+
+= SYNOPSIS
+
+In your dist.ini:
+
+  [RunExtraTests]
+
+= DESCRIPTION
+
+Runs xt tests when C<dzil test> is run. C<xt/release>, C<xt/author>, and
+C<xt/smoke> will be tested based on the values of the appropriate environment
+variables (C<RELEASE_TESTING>, C<AUTHOR_TESTING>, and C<AUTOMATED_TESTING>),
+which are set by C<dzil test>.
+
+= SEE ALSO
+
+* [Dist::Zilla]
+
+=end wikidoc
+
+=cut
+
