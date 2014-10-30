441ee434af118fa7f0f0b3819a53d1dbef12c4ef
David Golden <dagolden@cpan.org>
2014-10-30 16:25:21 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/Plugin/CheckExtraTests.pm
+++ lib/Dist/Zilla/Plugin/CheckExtraTests.pm
@@ -4,7 +4,7 @@ use warnings;
 package Dist::Zilla::Plugin::CheckExtraTests;
 # ABSTRACT: check xt tests before release
 
-our $VERSION = '0.023';
+our $VERSION = '0.024';
 
 # Dependencies
 use Dist::Zilla 4.3 ();
