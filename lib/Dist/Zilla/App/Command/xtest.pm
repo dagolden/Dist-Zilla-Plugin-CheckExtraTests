5d242df114dba82a290afe6b88c53ff1aede0c7a
David Golden <dagolden@cpan.org>
2015-08-10 10:53:10 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/App/Command/xtest.pm
+++ lib/Dist/Zilla/App/Command/xtest.pm
@@ -4,7 +4,7 @@ use warnings;
 package Dist::Zilla::App::Command::xtest;
 # ABSTRACT: run xt tests for your dist
 
-our $VERSION = '0.028';
+our $VERSION = '0.029';
 
 use Dist::Zilla::App -command;
 
--- lib/Dist/Zilla/Plugin/CheckExtraTests.pm
+++ lib/Dist/Zilla/Plugin/CheckExtraTests.pm
@@ -4,7 +4,7 @@ use warnings;
 package Dist::Zilla::Plugin::CheckExtraTests;
 # ABSTRACT: check xt tests before release
 
-our $VERSION = '0.028';
+our $VERSION = '0.029';
 
 # Dependencies
 use Dist::Zilla 4.3 ();
